252f1ee26438bfb655b348515903f9bf6a7d9297
kenash-dev <44662307+kenash-dev@users.noreply.github.com>
2024-10-08 22:16:37 +1100

    Add pagination on CRUD buttons.

--- src/Equinox.UI.Web/Controllers/CustomerController.cs
+++ src/Equinox.UI.Web/Controllers/CustomerController.cs
@@ -28,19 +28,19 @@ namespace Equinox.UI.Web.Controllers
         [HttpGet("customer-management/list-paginated")]
         public async Task<IActionResult> IndexPaginated(int pageNumber =1, int pageSize = 10)
         {
-            ViewData["pageNumber"] = pageNumber;
-            ViewData["pageSize"] = pageSize;
             var customerList = await _customerAppService.GetPaginatedList(pageNumber, pageSize);
             var totalCustomerCount = await _customerAppService.GetCustomerCount();
+
             return View(await PaginatedList<CustomerViewModel>.CreateAsync( customerList, totalCustomerCount, pageNumber, pageSize));
         }
 
         [AllowAnonymous]
         [HttpGet("customer-management/customer-details/{id:guid}")]
-        public async Task<IActionResult> Details(Guid? id)
+        public async Task<IActionResult> Details(Guid? id, int pageNumber)
         {
             if (id == null) return NotFound();
 
+            TempData["pageIndex"] = pageNumber;
             var customerViewModel = await _customerAppService.GetById(id.Value);
 
             if (customerViewModel == null) return NotFound();
@@ -57,7 +57,7 @@ namespace Equinox.UI.Web.Controllers
 
         [CustomAuthorize("Customers", "Write")]
         [HttpPost("customer-management/register-new")]
-        public async Task<IActionResult> Create(CustomerViewModel customerViewModel)
+        public async Task<IActionResult> Create(CustomerViewModel customerViewModel, int pageNumber)
         {
             if (!ModelState.IsValid) return View(customerViewModel);
             
@@ -65,13 +65,13 @@ namespace Equinox.UI.Web.Controllers
                 return View(customerViewModel);
 
             ViewBag.Sucesso = "Customer Registered!";
-
+            TempData["pageIndex"] = pageNumber;
             return View(customerViewModel);
         }
 
         [CustomAuthorize("Customers", "Write")]
         [HttpGet("customer-management/edit-customer/{id:guid}")]
-        public async Task<IActionResult> Edit(Guid? id)
+        public async Task<IActionResult> Edit(Guid? id, int pageNumber)
         {
             if (id == null) return NotFound();
 
@@ -79,12 +79,14 @@ namespace Equinox.UI.Web.Controllers
 
             if (customerViewModel == null) return NotFound();
 
+            TempData["pageIndex"] = pageNumber;
+
             return View(customerViewModel);
         }
 
         [CustomAuthorize("Customers", "Write")]
         [HttpPost("customer-management/edit-customer/{id:guid}")]
-        public async Task<IActionResult> Edit(CustomerViewModel customerViewModel)
+        public async Task<IActionResult> Edit(CustomerViewModel customerViewModel, int pageNumber)
         {
             if (!ModelState.IsValid) return View(customerViewModel);
             
@@ -92,13 +94,14 @@ namespace Equinox.UI.Web.Controllers
                 return View(customerViewModel);
 
             ViewBag.Sucesso = "Customer Updated!";
+            TempData["pageIndex"] = pageNumber;
 
             return View(customerViewModel);
         }
 
         [CustomAuthorize("Customers", "Remove")]
         [HttpGet("customer-management/remove-customer/{id:guid}")]
-        public async Task<IActionResult> Delete(Guid? id)
+        public async Task<IActionResult> Delete(Guid? id, int pageNumber)
         {
             if (id == null) return NotFound();
 
@@ -106,25 +109,32 @@ namespace Equinox.UI.Web.Controllers
 
             if (customerViewModel == null) return NotFound();
 
+            TempData["pageIndex"] = pageNumber;
+
             return View(customerViewModel);
         }
 
         [CustomAuthorize("Customers", "Remove")]
         [HttpPost("customer-management/remove-customer/{id:guid}"), ActionName("Delete")]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        public async Task<IActionResult> DeleteConfirmed(Guid id, int pageNumber)
         {
             if (ResponseHasErrors(await _customerAppService.Remove(id)))
                 return View(await _customerAppService.GetById(id));
 
             ViewBag.Sucesso = "Customer Removed!";
+
+            TempData["pageIndex"] = pageNumber;
+
             return RedirectToAction("Index");
         }
 
         [AllowAnonymous]
         [HttpGet("customer-management/customer-history/{id:guid}")]
-        public async Task<JsonResult> History(Guid id)
+        public async Task<JsonResult> History(Guid id, int pageNumber)
         {
             var customerHistoryData = await _customerAppService.GetAllHistory(id);
+            TempData["pageIndex"] = pageNumber;
+
             return Json(customerHistoryData);
         }
     }
--- src/Equinox.UI.Web/Models/PaginatedList.cs
+++ src/Equinox.UI.Web/Models/PaginatedList.cs
@@ -19,6 +19,8 @@ public class PaginatedList<T>: List<T>
 
     public bool HasNextPage => PageIndex < TotalPages;
 
+    //In microsoft documents the idea is to use the db-context model directly here and use EF core skip and take.
+    //Since we have layered architecture, we are passing the count, pageIndex and pageSize to determine the behaviour of Previous and Next buttons.
     public static async Task<PaginatedList<T>> CreateAsync(IEnumerable<T> items,int totalCustomerCount, int pageIndex, int pageSize)
     {
         return new PaginatedList<T>(items.ToList(), totalCustomerCount, pageIndex, pageSize);
